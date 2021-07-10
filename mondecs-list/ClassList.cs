0fcbda19303702e40420548da80cf7c70e2d9788
Akhil Remashan <34641800+AkhilRemashan@users.noreply.github.com>
2021-07-10 14:13:08 +0530

    cs-1007

---
+++ mondecs-list/ClassList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Hello
+{
+	public Hello()
+	{
+		Console.WriteLine("hey, there!");
+
+		Console.ReadLine();
+
+	}
+}
+
+public class DrawShape
+{
+	public DrawShape()
+	{
+		Console.WriteLine("   /|");
+		Console.WriteLine("  / |");
+		Console.WriteLine(" /  |");
+		Console.WriteLine("/___|");
+
+		Console.ReadLine();
+
+	}
+}
+
