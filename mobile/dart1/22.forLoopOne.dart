5d9dfdaf36468144a23670b11e418d60845aadd3
Akhil Remashan <34641800+AkhilRemashan@users.noreply.github.com>
2021-08-19 22:42:36 +0530

    1908-dart

---
+++ mobile/dart1/22.forLoopOne.dart
@@ -0,0 +1,15 @@
+import "dart:io";
+
+
+void main(){
+
+  
+    List<String> names = ["Jim", "Rough", "Skip", "Shaw", "Akil", "Sarr"];
+  
+    for(int i = 0; i < names.length; i++) {
+      print(
+        names[i]
+      );
+    }
+
+}
