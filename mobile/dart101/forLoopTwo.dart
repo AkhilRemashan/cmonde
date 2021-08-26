83b800f6006dc617c4526d7b1198945285c4819b
Akhil Remashan <34641800+AkhilRemashan@users.noreply.github.com>
2021-08-27 00:31:53 +0530

    2708-fltr

---
+++ mobile/dart101/forLoopTwo.dart
@@ -0,0 +1,13 @@
+import "dart:io";
+
+
+void main(){
+
+  
+    List<String> names = ["Jim", "Rough", "Skip", "Shaw", "Akil", "Sarr"];
+  
+    for(String name in names){
+      print(name);
+    }
+
+}
